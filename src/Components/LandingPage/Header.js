44c04a6aeec63494d40e687be3a560de8821cacf
Nico-Santos <santos.m.nicolas@gmail.com>
2021-04-16 16:45:26 -0300

    Search Field added

--- src/Components/LandingPage/Header.js
+++ src/Components/LandingPage/Header.js
@@ -5,8 +5,18 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Typography from '@material-ui/core/Typography';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    }
+  },
   toolbar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
@@ -27,9 +37,16 @@ export default function Header(props) {
   const classes = useStyles();
   const { title } = props;
 
+  const handleSearchClick = (e) => {
+    const text = document.getElementById("standard-basic").value
+    alert(text)
+    window.location.reload(false);
+  }
+
   return (
     <React.Fragment>
       <Toolbar className={classes.toolbar}>
+        <DehazeIcon />
         <Typography
           component="h2"
           variant="h5"
@@ -40,9 +57,12 @@ export default function Header(props) {
         >
           {title}
         </Typography>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
+        <Button onClick={handleSearchClick}>
+            <SearchIcon />
+        </Button>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Search" /> 
+        </form>
       </Toolbar>
     </React.Fragment>
   );
