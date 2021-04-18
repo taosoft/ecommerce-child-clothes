9241941ed5a4a1d2bed112386d2f2c9dcbae1a6b
Nico-Santos <santos.m.nicolas@gmail.com>
2021-04-18 19:19:27 -0300

    Hace redirect en grilla productos y featuredpost

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,7 +11,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { Redirect } from "react-router-dom";
+
 
 
 function Copyright() {
--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import ProductGrid from "./components/ProductList/ProductGrid";
-import TestProducts from './components/ProductList/TestProducts'
-import SingleTestProduct from './components/ProductList/SingleTest'
+import SingleProduct from './components/ProductList/SingleProduct';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
@@ -13,8 +12,7 @@ function Router() {
       <BrowserRouter>
         <Switch>
             <Route path="/products" component={ProductGrid}/>
-            <Route path="/product/:id" component={SingleTestProduct}/>
-            {/* <Route path="/test" component={TestProducts}/> */}
+            <Route path="/product/:id" component={SingleProduct}/>
             <Route path="/dashboard" component={Dashboard}/>
             <Route path="/login" component={Login}/>
             <Route path="/singup" component={SignUp}/>
