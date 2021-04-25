519dee835b2e46b396c1bc2b628db05135104168
Nico-Santos <santos.m.nicolas@gmail.com>
2021-04-25 15:56:12 -0300

    Delete CopyRight() in SingUp & Login

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -10,14 +10,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © Your Website ' + new Date().getFullYear() + '.'}
-    </Typography>
-  );
-}
-
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -118,9 +110,6 @@ export default function SignUp() {
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
