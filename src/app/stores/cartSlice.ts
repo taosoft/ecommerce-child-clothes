e6478614df4aca827a0a5f3e7544974624fd9514
Matias Grieben <matigrieben@frba.utn.edu.ar>
2021-05-02 12:55:41 -0300

    Added Cart Product store

---
+++ src/app/stores/cartSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import CartProduct from '../../models/cartProduct';
+
+interface CartState {
+  products: CartProduct[];
+}
+
+const initialState: CartState = {
+  products: []
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addCartProduct: (state, action: PayloadAction<CartProduct>) => {
+      state.products = [...state.products, action.payload];
+    }
+  },
+});
+
+export const { addCartProduct } = cartSlice.actions;
+
+export const selectCartCount = (state: RootState) => state.cart.products.length;
+export const selectCartProducts = (state: RootState) => state.cart.products;
+
+export default cartSlice.reducer;
