3bc77e688199d96654562f850e25b689578fbfb2
Nico-Santos <santos.m.nicolas@gmail.com>
2021-05-14 23:54:28 -0300

    WIP

--- backend/auth/authorization.js
+++ backend/auth/authorization.js
@@ -1,18 +1,18 @@
- var jwt = require('jsonwebtoken');
- var config = require('../config').config();
+ const jwt = require('jsonwebtoken');
+ const config = require('../config').config();
  
- var authorization = function (req, res, next) {
+ const authorization = function (req, res, next) {
  
-     var token = req.headers['x-access-token'];
+     const token = req.headers['x-access-token'] || req.headers['authorization'];
      console.log("token",token);
-     var msg = {auth: false, message: 'No token provided.'};
+     const msg = {auth: false, message: 'No token provided.'};
      if (!token)
          res.status(500).send(msg);
  
      let sec = process.env.SECRET;
      //console.log("secret",sec)
      jwt.verify(token, sec, function (err, decoded) {
-         var msg = {auth: false, message: 'Failed to authenticate token.'};
+         const msg = {auth: false, message: 'Failed to authenticate token.'};
          if (err)
          res.status(500).send(msg);
          req.userId = decoded.id;
