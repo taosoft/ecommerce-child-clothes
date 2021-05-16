015bd8f662d0f3fb9999693616fe432eda1ac3c9
Nico-Santos <santos.m.nicolas@gmail.com>
2021-05-16 13:14:19 -0300

    WIP

--- backend/auth/authorization.js
+++ backend/auth/authorization.js
@@ -1,20 +1,23 @@
  const jwt = require('jsonwebtoken');
- const config = require('../config').config();
+ require('../config').config();
  
- const authorization = function (req, res, next) {
+ const authorization = (req, res, next) => {
  
-     const token = req.headers['x-access-token'] || req.headers['authorization'];
-     const msg = {auth: false, message: 'No token provided.'};
-     
-     if (!token) res.status(500).send(msg);
+    const token = req.headers['x-access-token'] || req.headers['authorization'];
+    const msg = {auth: false, message: 'No token provided.'};
+    
+    if (!token) res.status(500).send(msg);
  
-     const sec = process.env.SECRET;
-     jwt.verify(token, sec, (err, decoded) => {
-        const msg = {auth: false, message: 'Failed to authenticate token.'};
-        if (err) res.status(500).send(msg);
-        req.userId = decoded.id;
-        next();
-     });
+    if(token.startsWith('Bearer ')) {
+        token = token.slice(7, token.length);
+        const key_word = process.env.SECRET;
+        jwt.verify(token, key_word, (err, decoded) => {
+            const msg = {auth: false, message: 'Failed to authenticate token.'};
+            if (err) res.status(500).send(msg);
+            req.userId = decoded.id;
+            next();
+        });
+    }
  }
  
  module.exports = authorization;
