d8b19d59f05c531c5597ff24b636237b448f9252
Nico-Santos <santos.m.nicolas@gmail.com>
2021-05-26 15:06:43 -0300

    DB Connection to localhost

--- backend/services/sales.service.js
+++ backend/services/sales.service.js
@@ -1,5 +1,5 @@
 // Gettign the Newly created Mongoose Model we just created
-const { Sale } = require("../models/Sale.module");
+const Sale = require("../models/Sale.module");
 
 // Saving the context of this module inside the _the variable
 _this = this;
