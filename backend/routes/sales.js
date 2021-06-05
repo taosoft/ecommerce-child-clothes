e04d3d947ceffbce5ee9dd7899c53f8dca4ab617
Nico-Santos <santos.m.nicolas@gmail.com>
2021-06-05 19:22:50 -0300

    cambio var por const

--- backend/routes/sales.js
+++ backend/routes/sales.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var router = express.Router();
-var SaleController = require("../controllers/sales.controller");
+const express = require("express");
+const router = express.Router();
+const SaleController = require("../controllers/sales.controller");
 
 // Authorize each API with middleware and map to the Controller Functions
 /* GET users listing. */
