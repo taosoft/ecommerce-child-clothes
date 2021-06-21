5fb1741f4b6aa24560f5680fb07bd74aa4c31814
Nico-Santos <santos.m.nicolas@gmail.com>
2021-06-21 15:40:00 -0300

    Mapa agregado

--- frontend/src/Components/LandingPage/AboutUs.js
+++ frontend/src/Components/LandingPage/AboutUs.js
@@ -5,6 +5,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import Header from './Header';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 
 function Copyright() {
   return (
@@ -52,6 +54,17 @@ export default function StickyFooter() {
             Somos una empresa en pleno crecimiento y con grandes expectativas. Nuestro propósito es ser la empresa más grande de ropa de niños en América Latina, otorgando calidad y precios accesibles.
         </Typography>
         <Typography variant="body1">Sticky footer placeholder.</Typography>
+      </Container>
+      <Container>
+        <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.478845883657!2d-58.383995984687594!3d-34.61733766572608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccad7e0f04d05%3A0x5e346deecfd0864!2sLima%20775%2C%20C1073AAO%20CABA!5e0!3m2!1sen!2sar!4v1624299596725!5m2!1sen!2sar"
+            width="100%"
+            height="300"
+            style={{ border: 0, display: "block", margin: "0" }}
+            allowfullscreen=""
+            loading="lazy">
+
+        </iframe>
       </Container>
       <footer className={classes.footer}>
         <Container maxWidth="sm">
