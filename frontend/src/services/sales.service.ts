bea56549fb04da5b771b01bbf6c5aa651c5ffb32
Nico-Santos <santos.m.nicolas@gmail.com>
2021-06-29 22:06:24 -0300

    WIP

--- frontend/src/services/sales.service.ts
+++ frontend/src/services/sales.service.ts
@@ -6,7 +6,7 @@ const baseUrl = process.env.REACT_APP_BASE_URL || "";
 export async function getSales(token: string): Promise<any> {
   return await axios.get<Sale[]>(baseUrl + '/api/sales', {
     headers: {
-    'Authorization': `Basic ${token}` 
+      'authorization': `Bearer ${token}`
     }
   });
 }
